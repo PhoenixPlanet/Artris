6b1233ce004fe36f87059f27b69519774097910f
Taehwan Yu <ytw0113@gmail.com>
2020-01-07 12:04:57 +0900

    feat: Add Enums

---
+++ include/Enums/Enums.h
@@ -0,0 +1,40 @@
+#ifndef ARTRIS_SCREEN_ENUMS_H_
+#define ARTRIS_SCREEN_ENUMS_H_
+
+#include <stdint.h>
+
+namespace artris {
+
+enum class BlockType: uint8_t {
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+};
+
+enum class BlockState: uint8_t {
+    DEFAULT,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+enum class ThrButtonType: uint8_t {
+    LEFT,
+    RIGHT,
+    DOWN,
+    OTHER
+};
+
+enum class TextAllignType: uint8_t {
+    LEFT,
+    CENTER,
+    RIGHT
+};
+
+}
+
+#endif
