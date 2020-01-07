f044b017dc77a141025fafe1f7c3845d78fc8bf5
Taehwan Yu <ytw0113@gmail.com>
2020-01-07 12:05:14 +0900

    feat: Add Cavas class

--- include/Screen/Canvas.h
+++ include/Screen/Canvas.h
@@ -1,14 +1,29 @@
 #ifndef ARTRIS_SCREEN_CANVAS_H_
 #define ARTRIS_SCREEN_CANVAS_H_
 
+#include <stdint.h>
+
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-class Screen {
+namespace artris {
+
+class Canvas {
+public:
+    Canvas();
+    ~Canvas();
+
 private:
-    Adafruit_SSD1306 *display_;
+    const Adafruit_SSD1306 DISPLAY_;
+    const uint8_t WIDTH;
+    const uint8_t HEIGHT;
+    
+    bool is_background_white_;
+
 };
+
+}
 
 #endif
