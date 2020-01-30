04fc04323d66d834674ffb98f346933dea8914a4
Taehwan Yu <ytw0113@gmail.com>
2020-01-30 22:06:41 +0900

    chore: Update settings.json and Includes in main

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
 #include <Arduino.h>
+#include <Screen/Canvas.h>
+#include <Screen/TextManager.h>
 
 void setup() {
   // put your setup code here, to run once:
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
